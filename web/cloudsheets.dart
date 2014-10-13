8f8deb5ab64ef4d48cb025a00ff71a8bd0ecd191
Klaus Erber <klaus.erber@gmail.com>
2014-10-13 23:27:31 +0200

    Layout

--- web/cloudsheets.dart
+++ web/cloudsheets.dart
@@ -5,10 +5,18 @@ import 'package:dquery/dquery.dart';
 
 void main() {
   //Button.use();  
-    
+  Transition.use();
+  Collapse.use();
+  
+  
   $("#sidebarToggle").click((QueryEvent ev) {
     
     $("#sidebarContainer").toggle();    
+  });
+  
+  $("#importButton").click((QueryEvent ev) {
+    $("#filesInput")[0].style.display = "inline";
+    $("#uploadButton")[0].style.display = "inline";
   });
   
   
